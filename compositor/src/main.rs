318e18b86bf413ec25f17886f5ae3bf4f8b1be51
indegrowl <indegrowl.mail@gmail.com>
2026-07-13 12:01:21 +0530

    feat: Run the compositor as a wayland client

--- compositor/src/main.rs
+++ compositor/src/main.rs
@@ -2,8 +2,10 @@ use app::{Poll, PrePoll, Start, prelude::*};
 use io_ring::{Ring, RingSettings};
 use wayland::{ClientConnected, WaylandServer};
 
+mod client_window;
 mod protocols;
 
+use client_window::ClientWindow;
 use protocols::wl_registry::WlRegistryState;
 
 #[derive(State)]
@@ -11,19 +13,23 @@ struct Compositor {
     server: WaylandServer,
     ring: Ring,
     registry: WlRegistryState,
+    client_window: ClientWindow,
 }
 
 fn main() {
     let ring = Ring::new(RingSettings::default());
     let server = WaylandServer::new("wayland-2", ring.proxy());
+    let client_window = ClientWindow::new(ring.proxy(), 1080, 1240, "compositor");
 
     let mut app = App::new(Compositor {
         server,
         ring,
         registry: WlRegistryState::new(),
+        client_window,
     })
     .mount(wayland::server_module())
     .mount(io_ring::module())
+    .mount(client_window::module())
     .mount(protocols::wl_display::module())
     .mount(protocols::wl_registry::module())
     .mount(protocols::wl_callback::module())

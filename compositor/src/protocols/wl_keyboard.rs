25aa9a1ce0294610fe4457211cdc9e93f47b8119
Ayaskant Panigrahi <ayaskantp@mechasystems.com>
2026-07-21 11:53:39 +0530

    fix: Implement actual serial and timestamp
    Also fix unused var warnings

--- compositor/src/protocols/wl_keyboard.rs
+++ compositor/src/protocols/wl_keyboard.rs
@@ -1,3 +1,5 @@
+use std::time::Instant;
+
 use app::{RegisteredModule, prelude::*};
 use std::os::fd::{AsFd, OwnedFd};
 use wayland::{
@@ -28,6 +30,10 @@ pub struct WlKeyboardState {
     pub focused_client: Option<Handle<WlKeyboard>>,
     pub repeat_info: KbRepeatInfo,
     pub keymap_info: KbKeymapInfo,
+    /// Monotonically increasing serial for enter/leave/key events we generate.
+    #[lens(skip)]
+    serial: u32,
+    start_time: Instant,
 }
 
 impl WlKeyboardState {
@@ -43,7 +49,19 @@ impl WlKeyboardState {
                 fd: None,
                 size: 0,
             },
+            serial: 1,
+            start_time: Instant::now(),
         }
+    }
+
+    fn time_ms(&self) -> u32 {
+        self.start_time.elapsed().as_millis() as u32
+    }
+
+    fn next_serial(&mut self) -> u32 {
+        let s = self.serial;
+        self.serial = self.serial.wrapping_add(1);
+        s
     }
 
     pub fn retain_alive(&mut self) {
@@ -95,7 +113,7 @@ pub fn module<S>() -> impl RegisteredModule<Compositor, S> {
             compositor.seat.keyboard_state.retain_alive();
             match ev {
                 WlKeyboardEvent::Keymap {
-                    sender,
+                    sender: _,
                     format,
                     fd,
                     size,
@@ -111,19 +129,20 @@ pub fn module<S>() -> impl RegisteredModule<Compositor, S> {
                     };
                 }
                 WlKeyboardEvent::Enter {
-                    sender,
-                    serial,
-                    surface,
+                    sender: _,
+                    serial: _,
+                    surface: _,
                     keys,
                 } => {
+                    let state = &mut compositor.seat.keyboard_state;
+                    let serial = state.next_serial();
                     println!("in wl_keyboard enter {:?}", serial);
                     if let Some(surf) = &compositor.seat.pointer_state.focused_surface {
-                        for kb in &compositor.seat.keyboard_state.client_keyboards {
+                        for kb in &state.client_keyboards {
                             if kb.proxy.is_same_connection(&surf.proxy) {
-                                // Send to client's first keyboard handle
-                                kb.enter(*serial, surf, keys);
-                                compositor.seat.keyboard_state.focused_surface = Some(surf.clone());
-                                compositor.seat.keyboard_state.focused_client = Some(kb.clone());
+                                kb.enter(serial, surf, keys);
+                                state.focused_surface = Some(surf.clone());
+                                state.focused_client = Some(kb.clone());
                                 println!("in wl_keyboard enter client {:?}", kb);
                                 return;
                             }
@@ -131,71 +150,73 @@ pub fn module<S>() -> impl RegisteredModule<Compositor, S> {
                     }
                 }
                 WlKeyboardEvent::Leave {
-                    sender,
-                    serial,
-                    surface,
+                    sender: _,
+                    serial: _,
+                    surface: _,
                 } => {
+                    let state = &mut compositor.seat.keyboard_state;
+                    let serial = state.next_serial();
                     println!("in wl_keyboard leave {:?}", serial);
-                    if let Some(surf) = &compositor.seat.keyboard_state.focused_surface
-                        && let Some(kb) = &compositor.seat.keyboard_state.focused_client
+                    if let Some(surf) = &state.focused_surface
+                        && let Some(kb) = &state.focused_client
                     {
-                        kb.leave(*serial, surf);
-                        compositor.seat.keyboard_state.focused_surface = None;
-                        compositor.seat.keyboard_state.focused_client = None;
+                        kb.leave(serial, surf);
+                        state.focused_surface = None;
+                        state.focused_client = None;
                         println!("in wl_keyboard leave client {:?}", serial);
                     }
                 }
                 WlKeyboardEvent::Key {
-                    sender,
-                    serial,
-                    time,
+                    sender: _,
+                    serial: _,
+                    time: _,
                     key,
-                    state,
+                    state: keystate,
                 } => {
-                    println!("in wl_keyboard key {:?}: {:?} {:?}", serial, key, state);
-                    if let Some(kb) = &compositor.seat.keyboard_state.focused_client {
-                        kb.key(*serial, *time, *key, *state);
-                        println!("in wl_keyboard key client {:?}: {:?} {:?}", kb, key, state);
+                    let state = &mut compositor.seat.keyboard_state;
+                    let serial = state.next_serial();
+                    let time = state.time_ms();
+                    println!("in wl_keyboard key {:?}: {:?} {:?}", serial, key, keystate);
+                    if let Some(kb) = &state.focused_client {
+                        kb.key(serial, time, *key, *keystate);
+                        println!(
+                            "in wl_keyboard key client {:?}: {:?} {:?}",
+                            kb, key, keystate
+                        );
                     } else if let Some(surf) = &compositor.seat.pointer_state.focused_surface {
-                        for kb in &compositor.seat.keyboard_state.client_keyboards {
+                        for kb in &state.client_keyboards {
                             if kb.proxy.is_same_connection(&surf.proxy) {
-                                // Send to client's first keyboard handle
-                                // TODO Cache keys? Empty array or fixed length?
-                                kb.enter(*serial, surf, &[]);
-                                compositor.seat.keyboard_state.focused_surface = Some(surf.clone());
-                                compositor.seat.keyboard_state.focused_client = Some(kb.clone());
+                                kb.enter(serial, surf, &[]);
+                                state.focused_surface = Some(surf.clone());
+                                state.focused_client = Some(kb.clone());
                                 println!("in wl_keyboard enter client {:?}", kb);
 
-                                kb.key(*serial, *time, *key, *state);
+                                kb.key(serial, time, *key, *keystate);
                                 println!(
                                     "in wl_keyboard key client {:?}: {:?} {:?}",
-                                    kb, key, state
+                                    kb, key, keystate
                                 );
                             }
                         }
                     }
                 }
                 WlKeyboardEvent::Modifiers {
-                    sender,
-                    serial,
+                    sender: _,
+                    serial: _,
                     mods_depressed,
                     mods_latched,
                     mods_locked,
                     group,
                 } => {
+                    let state = &mut compositor.seat.keyboard_state;
+                    let serial = state.next_serial();
                     println!("in wl_keyboard modifiers {:?}", serial);
-                    for kb in &compositor.seat.keyboard_state.client_keyboards {
-                        kb.modifiers(
-                            *serial,
-                            *mods_depressed,
-                            *mods_latched,
-                            *mods_locked,
-                            *group,
-                        );
+                    for kb in &state.client_keyboards {
+                        kb.modifiers(serial, *mods_depressed, *mods_latched, *mods_locked, *group);
                     }
                 }
                 WlKeyboardEvent::RepeatInfo {
-                    sender,
+                    sender: _,
                     rate,
                     delay,
                 } => {

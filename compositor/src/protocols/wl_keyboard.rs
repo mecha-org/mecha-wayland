7d3a43b7d36371632651b795acde0428a5d3d2c5
Ayaskant Panigrahi <ayaskantp@mechasystems.com>
2026-07-21 12:20:15 +0530

    chore: Refactor to remove repeated code

--- compositor/src/protocols/wl_keyboard.rs
+++ compositor/src/protocols/wl_keyboard.rs
@@ -71,163 +71,178 @@ impl WlKeyboardState {
     pub fn on_capability_removed(&mut self) {
         self.client_keyboards.clear();
     }
+
+    pub fn send_initial_state(&self, kb: &Handle<WlKeyboard>) {
+        let KbRepeatInfo { rate, delay } = self.repeat_info;
+        kb.repeat_info(rate, delay);
+
+        if let KbKeymapInfo {
+            format,
+            fd: Some(fd),
+            size,
+        } = &self.keymap_info
+        {
+            kb.keymap(*format, fd.as_fd(), *size);
+        }
+    }
+
+    pub fn set_keymap(&mut self, format: WlKeyboardKeymapFormat, fd: &OwnedFd, size: u32) {
+        for kb in &self.client_keyboards {
+            kb.keymap(format, fd.as_fd(), size);
+        }
+        self.keymap_info = KbKeymapInfo {
+            format,
+            fd: fd.try_clone().ok(),
+            size,
+        };
+    }
+
+    pub fn set_repeat_info(&mut self, rate: i32, delay: i32) {
+        for kb in &self.client_keyboards {
+            kb.repeat_info(rate, delay);
+        }
+        self.repeat_info = KbRepeatInfo { rate, delay };
+    }
+
+    pub fn broadcast_modifiers(
+        &self,
+        serial: u32,
+        mods_depressed: u32,
+        mods_latched: u32,
+        mods_locked: u32,
+        group: u32,
+    ) {
+        for kb in &self.client_keyboards {
+            kb.modifiers(serial, mods_depressed, mods_latched, mods_locked, group);
+        }
+    }
+
+    pub fn focus_surface(
+        &mut self,
+        surf: &Handle<WlSurface>,
+        serial: u32,
+        keys: &[u8],
+    ) -> Option<Handle<WlKeyboard>> {
+        if let Some(kb) = self
+            .client_keyboards
+            .iter()
+            .find(|kb| kb.proxy.is_same_connection(&surf.proxy))
+        {
+            kb.enter(serial, surf, keys);
+            self.focused_surface = Some(surf.clone());
+            self.focused_client = Some(kb.clone());
+            println!("in wl_keyboard enter client {:?}", kb);
+            Some(kb.clone())
+        } else {
+            None
+        }
+    }
+
+    pub fn clear_focus(&mut self, serial: u32) {
+        if let Some(surf) = &self.focused_surface
+            && let Some(kb) = &self.focused_client
+        {
+            kb.leave(serial, surf);
+            self.focused_surface = None;
+            self.focused_client = None;
+            println!("in wl_keyboard leave client {:?}", serial);
+        }
+    }
 }
 
 pub fn module<S>() -> impl RegisteredModule<Compositor, S> {
     Module::<Compositor, _, _>::new()
-        .on(|compositor: &mut Compositor, ev: &WlSeatRequest| {
-            match ev {
-                WlSeatRequest::GetKeyboard { id, .. } => {
-                    if let Some(capabilities) = compositor.seat.capability
-                        && capabilities.contains(WlSeatCapability::Keyboard)
-                    {
-                        compositor
-                            .seat
-                            .keyboard_state
-                            .client_keyboards
-                            .push(id.clone());
-                        println!(
-                            "seat keyboard: {:?}",
-                            id.object_id().expect("live keyboard")
-                        );
-                        // Send all cached info like repeat rate etc.
-                        let KbRepeatInfo { rate, delay } =
-                            compositor.seat.keyboard_state.repeat_info;
-                        id.repeat_info(rate, delay);
-                        if let KbKeymapInfo {
-                            format,
-                            fd: Some(fd),
-                            size,
-                        } = &compositor.seat.keyboard_state.keymap_info
-                        {
-                            id.keymap(*format, fd.as_fd(), *size);
-                        }
-                    } else {
-                        // TODO Send WlSeatError - through WlDisplay
-                    }
+        .on(|compositor: &mut Compositor, ev: &WlSeatRequest| match ev {
+            WlSeatRequest::GetKeyboard { id, .. } => {
+                if let Some(capabilities) = compositor.seat.capability
+                    && capabilities.contains(WlSeatCapability::Keyboard)
+                {
+                    compositor
+                        .seat
+                        .keyboard_state
+                        .client_keyboards
+                        .push(id.clone());
+                    println!(
+                        "seat keyboard: {:?}",
+                        id.object_id().expect("live keyboard")
+                    );
+                    compositor.seat.keyboard_state.send_initial_state(id);
+                } else {
+                    // TODO Send WlSeatError - through WlDisplay
                 }
-                _ => (),
             }
+            _ => (),
         })
         .on(|compositor: &mut Compositor, ev: &WlKeyboardEvent| {
-            compositor.seat.keyboard_state.retain_alive();
+            let state = &mut compositor.seat.keyboard_state;
+            state.retain_alive();
             match ev {
                 WlKeyboardEvent::Keymap {
-                    sender: _,
-                    format,
-                    fd,
-                    size,
+                    format, fd, size, ..
                 } => {
                     println!("in wl_keyboard keymap");
-                    for kb in &compositor.seat.keyboard_state.client_keyboards {
-                        kb.keymap(*format, fd.as_fd(), *size);
-                    }
-                    compositor.seat.keyboard_state.keymap_info = KbKeymapInfo {
-                        format: *format,
-                        fd: Some(fd.try_clone().expect("Failed to clone fd")),
-                        size: *size,
-                    };
+                    state.set_keymap(*format, fd, *size);
                 }
-                WlKeyboardEvent::Enter {
-                    sender: _,
-                    serial: _,
-                    surface: _,
-                    keys,
-                } => {
-                    let state = &mut compositor.seat.keyboard_state;
+                WlKeyboardEvent::Enter { keys, .. } => {
                     let serial = state.next_serial();
                     println!("in wl_keyboard enter {:?}", serial);
-                    if let Some(surf) = &compositor.seat.pointer_state.focused_surface {
-                        for kb in &state.client_keyboards {
-                            if kb.proxy.is_same_connection(&surf.proxy) {
-                                kb.enter(serial, surf, keys);
-                                state.focused_surface = Some(surf.clone());
-                                state.focused_client = Some(kb.clone());
-                                println!("in wl_keyboard enter client {:?}", kb);
-                                return;
-                            }
-                        }
+                    if let Some(surf) = compositor.seat.pointer_state.focused_surface.clone() {
+                        state.focus_surface(&surf, serial, keys);
                     }
                 }
-                WlKeyboardEvent::Leave {
-                    sender: _,
-                    serial: _,
-                    surface: _,
-                } => {
-                    let state = &mut compositor.seat.keyboard_state;
+                WlKeyboardEvent::Leave { .. } => {
                     let serial = state.next_serial();
                     println!("in wl_keyboard leave {:?}", serial);
-                    if let Some(surf) = &state.focused_surface
-                        && let Some(kb) = &state.focused_client
-                    {
-                        kb.leave(serial, surf);
-                        state.focused_surface = None;
-                        state.focused_client = None;
-                        println!("in wl_keyboard leave client {:?}", serial);
-                    }
+                    state.clear_focus(serial);
                 }
                 WlKeyboardEvent::Key {
-                    sender: _,
-                    serial: _,
-                    time: _,
                     key,
                     state: keystate,
+                    ..
                 } => {
-                    let state = &mut compositor.seat.keyboard_state;
                     let serial = state.next_serial();
                     let time = state.time_ms();
                     println!("in wl_keyboard key {:?}: {:?} {:?}", serial, key, keystate);
-                    if let Some(kb) = &state.focused_client {
+                    if let Some(kb) = state.focused_client.clone() {
+                        // Already focused on client; just send event
                         kb.key(serial, time, *key, *keystate);
                         println!(
                             "in wl_keyboard key client {:?}: {:?} {:?}",
                             kb, key, keystate
                         );
-                    } else if let Some(surf) = &compositor.seat.pointer_state.focused_surface {
-                        for kb in &state.client_keyboards {
-                            if kb.proxy.is_same_connection(&surf.proxy) {
-                                kb.enter(serial, surf, &[]);
-                                state.focused_surface = Some(surf.clone());
-                                state.focused_client = Some(kb.clone());
-                                println!("in wl_keyboard enter client {:?}", kb);
-
-                                kb.key(serial, time, *key, *keystate);
-                                println!(
-                                    "in wl_keyboard key client {:?}: {:?} {:?}",
-                                    kb, key, keystate
-                                );
-                            }
+                    } else if let Some(surf) = compositor.seat.pointer_state.focused_surface.clone()
+                    // TODO Put at top
+                    {
+                        // Enter new client and send event
+                        if let Some(kb) = state.focus_surface(&surf, serial, &[]) {
+                            kb.key(serial, time, *key, *keystate);
+                            println!(
+                                "in wl_keyboard key client {:?}: {:?} {:?}",
+                                kb, key, keystate
+                            );
                         }
                     }
                 }
                 WlKeyboardEvent::Modifiers {
-                    sender: _,
-                    serial: _,
                     mods_depressed,
                     mods_latched,
                     mods_locked,
                     group,
+                    ..
                 } => {
-                    let state = &mut compositor.seat.keyboard_state;
                     let serial = state.next_serial();
                     println!("in wl_keyboard modifiers {:?}", serial);
-                    for kb in &state.client_keyboards {
-                        kb.modifiers(serial, *mods_depressed, *mods_latched, *mods_locked, *group);
-                    }
+                    state.broadcast_modifiers(
+                        serial,
+                        *mods_depressed,
+                        *mods_latched,
+                        *mods_locked,
+                        *group,
+                    );
                 }
-                WlKeyboardEvent::RepeatInfo {
-                    sender: _,
-                    rate,
-                    delay,
-                } => {
+                WlKeyboardEvent::RepeatInfo { rate, delay, .. } => {
                     println!("in wl_keyboard repeat_info {:?} {:?}", rate, delay);
-                    for kb in &compositor.seat.keyboard_state.client_keyboards {
-                        kb.repeat_info(*rate, *delay);
-                    }
-                    compositor.seat.keyboard_state.repeat_info = KbRepeatInfo {
-                        rate: *rate,
-                        delay: *delay,
-                    };
+                    state.set_repeat_info(*rate, *delay);
                 }
             }
         })
